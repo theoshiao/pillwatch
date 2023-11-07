106a05b4d88c6a14bf9d17c4c02fe937f5390c66
Amy Huang <amyxjhuang@berkeley.edu>
2023-11-06 23:16:50 -0500

    Form front-end"

--- frontend/pillwatch/src/app/components/Login.js
+++ frontend/pillwatch/src/app/components/Login.js
@@ -1,77 +1,51 @@
 "use client";
 import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import {loginUser} from './api';
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = async (e) => {
-    console.log(username, password);
-    setUsername("jldskfj");
+  // const handleSubmit = (e) => {
+  //   console.log(username, password);
+  //   setUsername("jldskfj");
 
-    e.preventDefault();
-    // Create a data object to send to the server
-    const data = {
-      username: username,
-      password: password,
-    };
+  //   e.preventDefault();
+  //   // Create a data object to send to the server
+  //   const data = {
+  //     username: username,
+  //     password: password,
+  //   };
 
-    try {
-      const response = await fetch("http://127.0.0.1:5000/api/register", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-      console.log(response.json);
-      if (response.ok) {
-        // Registration was successful
-        // You can redirect to a different page or show a success message
-      } else {
-        // Handle registration errors, such as duplicate username or invalid password
-        // You can display an error message to the user
-      }
-    } catch (error) {
-      console.error("Error during registration:", error);
-    }
-  };
-
-  const loginUser = async () => {
-    const data = {
-        username: "username",
-        password: "password",
-      };
-    try {
-        const response = await fetch("http://127.0.0.1:5000/api/register", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        });
-        console.log(response.json);
-        if (response.ok) {
-          // Registration was successful
-          // You can redirect to a different page or show a success message
-        } else {
-          // Handle registration errors, such as duplicate username or invalid password
-          // You can display an error message to the user
-        }
-      } catch (error) {
-        console.error("Error during registration:", error);
-      }
-  }
-  const message = loginUser();
+  //   try {
+  //     const response = await fetch("http://127.0.0.1:5000/api/register", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(data),
+  //     });
+  //     console.log(response.json);
+  //     if (response.ok) {
+  //       // Registration was successful
+  //       // You can redirect to a different page or show a success message
+  //     } else {
+  //       // Handle registration errors, such as duplicate username or invalid password
+  //       // You can display an error message to the user
+  //     }
+  //   } catch (error) {
+  //     console.error("Error during registration:", error);
+  //   }
+  // };
 
   const onClick = (e) => {
-    console.log(this.state);
+    loginUser(username, password);
   };
 
   return (
     <div>
-        {message}
+        {/* {message} */}
       <Form onSubmit={onClick}>
         <Form.Label>Username</Form.Label>
         <Form.Control
@@ -81,7 +55,6 @@ export default function Login() {
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-        {username} {password}
         <Form.Label>Password</Form.Label>
         <Form.Control
           id="form-password"
@@ -94,7 +67,6 @@ export default function Login() {
           Register
         </Button>
       </Form>
-      <Button onClick={() => {console.log("hello")}} />
     </div>
   );
 }
