bcaa58c87122a7315fd1f251c7dab1a79c0054d5
Amy Huang <amyxjhuang@berkeley.edu>
2023-11-07 19:28:57 -0500

    Submit image and get response

--- backend/db.py
+++ backend/db.py
@@ -2,7 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from config import * 
 from flask_login import LoginManager
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+# Allow requests only from 'http://localhost:3000'
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+
 app.config["SQLALCHEMY_DATABASE_URI"] = AZURE_POSTGRESQL_CONNECTIONSTRING
 db = SQLAlchemy()
 db.init_app(app)
