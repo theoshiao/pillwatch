a0faab19b7e92c0c681d04248f952b47cc25f99a
Amy Huang <amyxjhuang@berkeley.edu>
2023-11-14 18:51:42 -0500

    enabled CORS for all routes

--- backend/db.py
+++ backend/db.py
@@ -2,7 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from config import * 
 from flask_login import LoginManager
+from flask_cors import CORS
+
 app = Flask(__name__)
+
+# Enable CORS for all routes
+CORS(app)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = AZURE_POSTGRESQL_CONNECTIONSTRING
 db = SQLAlchemy()
 db.init_app(app)
