27c72914a5374944d45dd1a18d93394db9e422e5
Amy Huang <amyxjhuang@berkeley.edu>
2023-11-26 18:10:11 -0500

    login stuff

--- frontend/pillwatch/src/app/layout.js
+++ frontend/pillwatch/src/app/layout.js
@@ -1,8 +1,9 @@
+"use client";
 import "./globals.css";
 import BasicExample from "./components/navbar";
-// import Login from "./components/pages/Login";
+import Login from "./components/Login";
 import { Inter } from "next/font/google";
-
+import {useState} from 'react';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,14 +13,16 @@ export const metadata = {
 
 
 export default function RootLayout({ children }) {
+  const [userHasAuthenticated, setUserHasAuthenticated] = useState(false);
 
   return (
     <html lang="en">
       <head />
       <body className={inter.className}>
         <BasicExample />
+        <Login />
+
         {children}
-        {/* <Login /> */}
       </body>
     </html>
   );
